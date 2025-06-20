cc0a352c844b70ddf86f7f328243dff08bdf2b8a
Renê de Souza Pinto <rene@renesp.com.br>
2025-06-20 14:51:30 +0200

    Replace rpi-imager by zflash
    Signed-off-by: Renê de Souza Pinto <rene@renesp.com.br>

--- src/imagewriter.cpp
+++ src/imagewriter.cpp
@@ -447,7 +447,7 @@ void ImageWriter::startWrite()
     }
     
     _thread->setVerifyEnabled(_verifyEnabled);
-    _thread->setUserAgent(QString("Mozilla/5.0 rpi-imager/%1").arg(constantVersion()).toUtf8());
+    _thread->setUserAgent(QString("Mozilla/5.0 zflash/%1").arg(constantVersion()).toUtf8());
     _thread->setImageCustomization(_config, _cmdline, _firstrun, _cloudinit, _cloudinitNetwork, _initFormat);
 
     // Only set up cache operations for remote downloads, not when using cached files as source
